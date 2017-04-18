c88f0c4e9fb28d8a4a516fd986155b9249283792
Richard Hill III <rhillz718@gmail.com>
2017-04-18 17:28:11 -0400

    added api routes basic layout and also added user authentication to authRoutes

---
+++ authRoutes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.router();
+const parser = require('body-parser');
+
+router.use(parser.json());
+
+
+router.get('/posts', (request, response, next) => {
+
+
+    next();
+});
+
+
+router.post('/posts', (request, response, next) => {
+
+
+    next();
+});
+
+router.put('/post/:id', (request, response, next) => {
+    const id = parseInt(request.params.id);
+
+    next();
+});
+
+router.delete('/post/:id', (request, response, next) => {
+    const id = parseInt(request.params.id);
+
+    next();
+});
+
+
+
+router.use((request, response) => {
+    response.header('Content-Type', 'application/json');
+    response.send(data)
+});
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3088, () => {
+    console.log('Exchangagram Live!')
+})
