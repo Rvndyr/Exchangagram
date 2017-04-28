9c3ea172a5839ba64470945494e41eaad0af2550
randyr2125 <Randyr2125@msn.com>
2017-04-27 21:41:40 -0400

    fixed fe2

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,135 +1,230 @@
 const express = require('express');
-const router = express.Router();
+const router = express();
 const parser = require('body-parser');
 const exchanger = require('./exchanger');
 const db = require('sqlite');
 const DB_NAME = './database.sqlite';
 
-router.use(parser.json());
+// router.use(parser.json());
+
+
+
+
+// SIGN UP
 
 // GET ROUTES
 
-router.get('/posts', (request, response, next) => {
-  exchanger.getUser(request, response).then((data) => {
-  response.header('Content-Type', 'application/json');
-  response.send({ users: data });
-  }).catch((e) => {
-      response.status(401);
-  });
-
-    next();
-});
-
-// added this example for you to work off of rich
-router.get('/users', (request, response, next) => {
-    exchanger.getUsers(request, response).then((data) => {
-    response.header('Content-Type', 'application/json');
-    response.send({ users: data });
+router.get('/feed/:user_id', (request, response) => {
+    exchanger.getActivity(request.params.user_id).then((data) => {
+        response.header('Content-Type', 'application/json');
+        response.send({ activity: data });
     }).catch((e) => {
         response.status(401);
     });
-
-
-   // next(); had to comment out rich to get /users to work
-
 });
 
-router.get('/users/:id', (request, response, next) => {
-    const id = parseInt(request.params.id, 10);
-
-
-    next()
+// get all users
+router.get('/users', (request, response) => {
+    exchanger.getUsers(request, response).then((data) => {
+        response.header('Content-Type', 'application/json');
+        response.send({ users: data });
+    }).catch((e) => {
+        response.status(401);
+    });
 });
 
-router.get('/followers/:user_id', (request, response, next) => {
-    const id = parseInt(request.params.id, 10);
 
-    next();
-})
-
-router.get('/followers/:follower_id', (request, response, next) => {
-    const id = parseInt(request.params.id, 10);
-
-    next();
-})
-
-router.get('/activities', (request, response, next) => {
-
-    next();
-})
-
-router.get('/activities/:user_id', (request, response, next) => {
-    const id = parseInt(request.params.id, 10);
-
-    next();
-})
-
-// POST ROUTES
-
-router.post('/posts', (request, response, next) => {
-
-
-    next();
+// Get a specified user and activties of user_id
+router.get('/user/:user_id', (req, res, next) => {
+    const id = parseInt(req.params.user_id, 10);
+    exchanger.getUser(id)
+        .then((data) => {
+            res.header('Content-Type', 'application/json');
+            res.send({
+                user: data
+            });
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(401);
+        });
 });
 
-router.post('/activities', (request, response, next) => {
-
-
-    next();
+// get users that $user_id follows
+router.get('/:user_id/followedusers', (req, res) => {
+    const id = parseInt(req.params.user_id, 10);
+    exchanger.getFollowed(id)
+        .then((data) => {
+            res.header('Content-Type', 'application/json');
+            res.send({
+                followed_users: data
+            });
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(401);
+        });
 });
 
-router.post('/followers', (request, response, next) => {
-
-
-    next();
+// Follow a user ##url feeds the user_id + followed_id.
+router.post('/:user_id/follow/:followed_id', (req, res, next) => {
+    const user_id = parseInt(req.params.user_id, 10);
+    const followed_id = parseInt(req.params.followed_id, 10);
+    exchanger.followUser(user_id, followed_id)
+        .then((data) => {
+            res.header('Content-Type', 'application/json');
+            res.send({
+                followed_users: data
+            });
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(401);
+        });
 });
 
-router.post('/users', (request, response, next) => {
-
-
-    next();
+// Unfollow a user ## feeds the user_id + followed_id.
+router.delete('/:user_id/unfollow/:followed_id', (req, res, next) => {
+    const user_id = parseInt(req.params.user_id, 10);
+    const followed_id = parseInt(req.params.followed_id, 10);
+    exchanger.unfollow(user_id, followed_id)
+        .then((data) => {
+            res.header('Content-Type', 'application/json');
+            res.send({
+                followed_users: data
+            });
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(401);
+        });
 });
 
-// PUT ROUTES
 
-router.put('/post/:id', (request, response, next) => {
-    const id = parseInt(request.params.id, 10);
-
-    next();
+// create a post. ##url feeds the user_id + followed_id. req targets activity table columns
+router.post('/:user_id/post', (req, res, next) => {
+    let args = {};
+    for (const prop in req.body) {
+        args['$' + prop] = req.body[prop];
+    }
+    req.body = args;
+    const user_id = parseInt(req.params.user_id, 10);
+    exchanger.createActivity(user_id, req.body)
+        .then((data) => {
+            res.header('Content-Type', 'application/json');
+            res.send({
+                post: data
+            });
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(401);
+        });
 });
 
-router.put('/users/:id', (request, response, next) => {
-    const id = parseInt(request.params.id, 10);
-
-    next();
+// Get a specified post ## url feeds post_id
+router.get('/post/:post_id', (req, res, next) => {
+    const id = parseInt(req.params.post_id, 10);
+    exchanger.getPost(id)
+        .then((data) => {
+            res.header('Content-Type', 'application/json');
+            res.send({
+                user: data
+            });
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(401);
+        });
 });
 
-// DELETE ROUTES
+// router.get('/followers/:user_id', (request, response, next) => {
+//     const id = parseInt(request.params.id, 10);
 
-router.delete('/post/:id', (request, response, next) => {
-    const id = parseInt(request.params.id, 10);
+//     next();
+// })
 
-    next();
+// router.get('/followers/:follower_id', (request, response, next) => {
+//     const id = parseInt(request.params.id, 10);
+
+//     next();
+// })
+
+// router.get('/activities', (request, response, next) => {
+
+//     next();
+// })
+
+// router.get('/activities/:user_id', (request, response, next) => {
+//     const id = parseInt(request.params.id, 10);
+
+//     next();
+// })
+
+// // POST ROUTES
+
+// router.post('/posts', (request, response, next) => {
+
+
+//     next();
+// });
+
+// router.post('/activities', (request, response, next) => {
+
+
+//     next();
+// });
+
+// router.post('/followers', (request, response, next) => {
+
+
+//     next();
+// });
+
+// router.post('/users', (request, response, next) => {
+
+
+//     next();
+// });
+
+// // PUT ROUTES
+
+// router.put('/post/:id', (request, response, next) => {
+//     const id = parseInt(request.params.id, 10);
+
+//     next();
+// });
+
+// router.put('/users/:id', (request, response, next) => {
+//     const id = parseInt(request.params.id, 10);
+
+//     next();
+// });
+
+// // DELETE ROUTES
+
+// had to comment this out rich or public would not serv
+//     next();
+// });
+
+// router.delete('/users/:id', (request, response, next) => {
+//     const id = parseInt(request.params.id, 10);
+
+//     next();
+// });
+
+// router.delete('/followers/:followers_id', (request, response, next) => {
+//     const id = parseInt(request.params.id, 10);
+
+//     next();
+// });
+
+// // middle ware
+
+// // had to comment this out rich or public would not serve
+router.use((request, response) => {
+    response.header('Content-Type', 'application/json')
+    response.send(data);
 });
-
-router.delete('/users/:id', (request, response, next) => {
-    const id = parseInt(request.params.id, 10);
-
-    next();
-});
-
-router.delete('/followers/:followers_id', (request, response, next) => {
-    const id = parseInt(request.params.id, 10);
-
-    next();
-});
-
-// middle ware
-
-// had to comment this out rich or public would not serve
-/* router.use((request, response) => {
-    response.header('Content-Type', 'application/json');
-    response.send(data)
-}); */
 
 module.exports = router; // had to change this from apiRouter to router or code wouldnt work i think it has to do with line 3
--- authRoutes.js
+++ authRoutes.js
@@ -2,32 +2,28 @@ const express = require('express');
 const parser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
+const exchanger = require('./exchanger');
+const expressSession = require('express-session');
+// const FacebookStrategy = require('passport-facebook').Strategy;
+
+// const app = express();
+const router = express();
+
+router.use(parser.json());
+// router.use('/', express.static('./public'));
+
+
+// router.use(expressSession({
+//     secret: 'HUSH'
+// }));
+
+// router.use(expressSession({
+//     secret: 'FOBAR'
+// }));
 
 
 
-const app = express();
 
-app.use(parser.json());
-
-app.use('/', express.static('/public'));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-app.get('/api/posts/:id', (request, response) => {
-    response.header('content-type', 'application/json');
-    response.send({
-
-        "success": false
-    });
-
-    response.send({
-        "success": True
-    });
-
-});
 
 
 passport.serializeUser((user, done) => {
@@ -38,8 +34,6 @@ passport.deserializeUser((user, done) => {
 
     done(null, user)
 });
-
-
 
 passport.use(new LocalStrategy({
     usernameField: 'email',
@@ -54,7 +48,13 @@ passport.use(new LocalStrategy({
 
 
 
-app.post('/auth/login', (request, response, next) => {
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+// LOG IN
+router.post('/auth/login', (request, response, next) => {
+    console.log("here")
     passport.authenticate('local', (err, user, info) => {
         if (err) console.log(err);
         if (!user) console.log(user);
@@ -65,9 +65,23 @@ app.post('/auth/login', (request, response, next) => {
             response.header('Content-Type', 'application/json');
             response.send({
                 success: true,
+                // userId: user.id
             });
         });
     })(request, response, next);
 });
 
-module.exports = app;
+
+router.use((request, response, next) => {
+    console.log('in authRoutes')
+    console.log(request.session, request.user)
+    if (request.isAuthenticated()) {
+        next();
+    } else {
+        response.status(403)
+        response.send({ success: false })
+    }
+})
+
+
+module.exports = router;
