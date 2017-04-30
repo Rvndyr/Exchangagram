4c4d8193338cca62c243d1a5c3338c037023d2a9
Richard Hill III <rhillz718@gmail.com>
2017-04-29 20:28:38 -0400

    user auth fixed, now rendering users to user page

--- exchanger.js
+++ exchanger.js
@@ -26,10 +26,10 @@ exchanger.getActivities = (user_id) => {
 };
 
 // get single user activity
-exchanger.getActivity = (user_id, activity_payload) => {
-    return db.run(`SELECT activity_payload From activities
-                    WHERE activities.user_id = ${user_id} and activity_payload =${activity_payload}`);
-}
+// exchanger.getActivity = (user_id, activity_payload) => {
+//     return db.run(`SELECT activity_payload From activities
+//                     WHERE activities.user_id = ${user_id} and activity_payload =${activity_payload}`);
+// }
 
 // Get a specified user via user.id + their activity
 exchanger.getUser = (user_id) => {
@@ -43,7 +43,7 @@ exchanger.getFollowed = (user_id) => {
     return db.all(`SELECT 
                 users.name AS name,
                 users.email AS email,
-                activities.activity_id AS activity_id,
+                activities.activity_type_id AS activity_type_id,
                 activities.activity_payload AS payload
             FROM users
                 INNER JOIN followers ON followers.followed_id = users.id 
@@ -51,14 +51,14 @@ exchanger.getFollowed = (user_id) => {
             WHERE followers.user_id = ${user_id}`)
 };
 
-// Get a specified payload via activities.activity_id
-exchanger.getPost = (activity_id) => {
+// Get a specified payload via activities.activity_type_id
+exchanger.getPost = (activity_type_id) => {
     return db.all(`SELECT
                     users.name AS name,
                     activities.activity_payload AS payload
                 FROM activities
                     INNER JOIN users ON activities.user_id = users.id
-                WHERE activities.activity_id = ${activity_id}`)
+                WHERE activities.activity_type_id = ${activity_type_id}`)
 };
 
 
@@ -78,8 +78,8 @@ exchanger.followUser = (user_id, followed_id) => {
 };
 
 // Edit a activity payload
-exchanger.updatePost = (user_id, activity_id, updatedText) => {
-    return db.run(`UPDATE activities SET activity_payload = "${updatedText}" WHERE activity_id = ${activity_id} and user_id = ${user_id}`)
+exchanger.updatePost = (user_id, activity_type_id, updatedText) => {
+    return db.run(`UPDATE activities SET activity_payload = "${updatedText}" WHERE activity_type_id = ${activity_type_id} and user_id = ${user_id}`)
 };
 
 // Delete a acitivity paylaod
